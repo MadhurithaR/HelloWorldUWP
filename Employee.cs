9550fdbabebda7b3b3e36239a83d0c1a848ba2ba
MadhurithaR <37748124+MadhurithaR@users.noreply.github.com>
2018-07-04 15:02:15 -0700

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Employee
+    {
+        //Name of Employee
+        public string Name { get; set; }
+        //Title of Employee
+        public string Title { get; set; }
+
+        public static Employee GetEmployee()
+        {
+
+            var emp = new Employee
+            {
+                Name = "John Doe",
+                Title = "Director of Engineering"
+            };
+            return emp;
+        }
+    }
+}
+        
+    
+
